9aa668d59c65904ed9a20a4ed2bf1c131e7af76f
unknown <2224290077@ACADEMICO.IESB.BR>
2023-06-02 20:29:24 -0300

    Atualizado

--- navegacao/src/contexts/UserContext.jsx
+++ navegacao/src/contexts/UserContext.jsx
@@ -1,21 +1,36 @@
 import { createContext, useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB24hUkVyXK_vXDP92iBcHpcjsy04Utq8g",
+    authDomain: "back-end-react2.firebaseapp.com",
+    projectId: "back-end-react2",
+    storageBucket: "back-end-react2.appspot.com",
+    messagingSenderId: "841571748550",
+    appId: "1:841571748550:web:cc8a00cf114d6f585ee5c7"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth={app};
 
 const UserContext = createContext({
     userID: null,
     logado: false,
-    handleLogin: () => {},
-    handleLogout: () => {},
+    handleLogin: () => { },
+    handleLogout: () => { },
 })
 
 export function UserContextProvider(props) {
-    const [currentUser, setCurrentUser] = useState({userID: null, logado: false})
-    
+    const [currentUser, setCurrentUser] = useState({ userID: null, logado: false })
+
     function login() {
-        setCurrentUser({userID:100, logado:true})
+        setCurrentUser({ userID: 100, logado: true })
     }
 
     function logout() {
-        setCurrentUser({userID:null, logado:false})
+        setCurrentUser({ userID: null, logado: false })
     }
     const contexto = {
         userID: currentUser.userID,
